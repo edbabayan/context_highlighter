5910ac47f090108c6a40a462d299ac33b03d681b
edbabayan <ebabayan86@gmail.com>
2025-06-23 14:42:33 +0400

    ADD: docstring

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import json
 
+
 def load_table_metadata(file_path):
+    """
+    Load table metadata from a JSON file to avoid running the extraction pipeline repeatedly.
+    """
     with open(file_path, "r") as f:
         saved_tables = json.load(f)
     return saved_tables
