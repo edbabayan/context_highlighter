48a8b94406393ccb2bd95b17eb783e59f581867b
edbabayan <ebabayan86@gmail.com>
2025-06-30 23:01:25 +0400

    ADD: new directions for evaluation

--- evaluation/config.py
+++ evaluation/config.py
@@ -4,6 +4,7 @@ class CFG:
     root = Path(__file__).resolve().parent.parent.absolute()
 
     data_dir = root.joinpath("data")
+    pdf_dir = data_dir.joinpath("pdfs")
     json_dir = data_dir.joinpath("json_files")
     
     # Output directories
