f73f5fdb8a182fd1deee91c991d92ab109fe6c71
edbabayan <ebabayan86@gmail.com>
2025-06-30 15:49:01 +0400

    ADD: new directories for preprocessing

--- evaluation/config.py
+++ evaluation/config.py
@@ -4,3 +4,8 @@ class CFG:
     root = Path(__file__).resolve().parent.parent.absolute()
 
     data_dir = root.joinpath("data")
+    json_dir = data_dir.joinpath("json_files")
+    
+    # Output directories
+    evaluation_dir = root.joinpath("evaluation")
+    processed_json_dir = data_dir.joinpath("processed_json")
