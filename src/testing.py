6ef7aba19bda7c6843e247e52adeb25d21f1c993
edbabayan <ebabayan86@gmail.com>
2025-07-01 14:27:58 +0400

    CHANGE: input sequence

--- src/testing.py
+++ src/testing.py
@@ -1,20 +1,19 @@
 from src.pymupdf_highlighter.row_highlighter import highlight_sentences_on_page
 from src.config import CFG
 
-_pdf_path = "/Users/eduard_babayan/Documents/epam_projects/context_highlighter/data/pdfs/02-15-2024-FR-(Final).pdf"
+_pdf_path = "/Users/eduard_babayan/Documents/epam_projects/context_highlighter/Document.pdf"
 
 sentences_to_highlight = [
-    "Disaster Assistance Loans - SBA",
-    "270.7"
+    "Financial statements"
 ]
 
-target_page = 91
+target_page = 20
 
 boxes = highlight_sentences_on_page(
-    _pdf_path, 
-    CFG.row_highlighter_pdf_path, 
+    _pdf_path,
     target_page,
-    sentences_to_highlight
+    sentences_to_highlight,
+CFG.row_highlighter_pdf_path,
 )
 
 print('')
